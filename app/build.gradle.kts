d25145f597df87c464b6e1604d1cc1def7f222c9
Junseok Seo <joshseo@mabopractice.com>
2024-06-02 10:51:36 +0900

    android setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,77 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kapt)
+}
+
+android {
+    namespace = "practice.omdb.junseokseo"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "practice.omdb.junseokseo"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField(
+            "String",
+            "REST_API_KEY",
+            gradleLocalProperties(rootDir, providers).getProperty("omdb_api_key")
+        )
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //hilt
+    implementation(libs.dagger.hilt)
+    kapt(libs.dagger.hilt.compiler)
+
+    //network
+    implementation(libs.okHttp)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.adapter)
+    implementation(libs.converter.gson)
+    implementation(libs.okHttp.interceptor)
+
+    //image
+    implementation(libs.coil)
+}
